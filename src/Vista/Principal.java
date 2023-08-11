c9407af9b49910ba55bf6d0c4c1200c5a59cc58a
JeanCHV <jc_26.10@hotmail.com>
2023-08-11 17:09:01 -0500

    Look and feel Tablas

--- src/Vista/Principal.java
+++ src/Vista/Principal.java
@@ -8,6 +8,11 @@ public class Principal {
 
     public static void main(String[] args) {
        frmLogin.main(args);
+                try {
+            javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
        
 //       ConexionBD cn = new ConexionBD();
 //       ConexionBD.conectar();
